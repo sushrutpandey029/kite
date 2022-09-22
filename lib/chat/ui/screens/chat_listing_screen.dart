623776ff6ed3063f8afe610ea52191714bc4088e
shrikantsahu2004 <sahu.sr@somaiya.edu>
2022-09-22 08:58:00 +0530

    Chat with changes

---
+++ lib/chat/ui/screens/chat_listing_screen.dart
@@ -0,0 +1,122 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:intl/intl.dart';
+import 'package:kite/authentication/model/auth_user_model.dart';
+import 'package:kite/authentication/provider/auth_provider.dart';
+import 'package:kite/profile/provider/update_profile_provider.dart';
+import 'package:kite/util/custom_navigation.dart';
+import 'package:provider/provider.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+import '../../provider/chat_provider.dart';
+import 'chat_screen.dart';
+
+class ChatListingPage extends StatefulWidget {
+  const ChatListingPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChatListingPage> createState() => _ChatListingPageState();
+}
+
+class _ChatListingPageState extends State<ChatListingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () async {
+        await context.read<ChatProvider>().fetchChatUsers(context);
+      },
+      child: Consumer<ChatProvider>(builder: (context, value, widget) {
+        return ListView.builder(
+          itemCount: value.chatUsersList.length,
+          itemBuilder: (context, index) {
+            String msgText = value.chatUsersList[index].lastMessage;
+            if (msgText == "") {
+              msgText = "Sent a File";
+            } else {
+              String fullMessage =
+                  value.chatUsersList.elementAt(index).lastMessage;
+              List<String> splitMessage = fullMessage.split("\n");
+              msgText =
+                  splitMessage.length > 1 ? splitMessage.first : fullMessage;
+            }
+
+            return Card(
+              margin: EdgeInsets.symmetric(horizontal: 4.w, vertical: 1.h),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              elevation: 4,
+              child: ListTile(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  onTap: () {
+                    value.selectUser(
+                        index,
+                        context,
+                        false,
+                        AuthUserModel(
+                          id: '',
+                          userRegNo: '',
+                          country: '',
+                          userPhoneNumber: '',
+                          userImage: '',
+                          userName: '',
+                          userBio: '',
+                          userDob: DateTime.now(),
+                          userCreateDateTime: DateTime.now(),
+                          userIpAddress: '',
+                          userPhoneName: '',
+                          userStatus: '',
+                          userFcm: '',
+                          userUid: '',
+                        ));
+                  },
+                  contentPadding: EdgeInsets.only(left: 2.w),
+                  tileColor: const Color.fromARGB(255, 231, 231, 231),
+                  leading: CircleAvatar(
+                    radius: 20.sp,
+                    child: const Icon(Icons.person),
+                  ),
+                  title: Text(value.chatUsersList.elementAt(index).userName),
+                  subtitle: Text(msgText),
+                  trailing: Padding(
+                    padding: EdgeInsets.only(right: 4.w),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          DateFormat.jm().format(value.chatUsersList
+                              .elementAt(index)
+                              .lastMessageTime),
+                          style: TextStyle(fontSize: 14.sp),
+                        ),
+                        SizedBox(
+                          height: 2.75.h,
+                          child: value.chatUsersList
+                                      .elementAt(index)
+                                      .unreadMessagesCount ==
+                                  0
+                              ? null
+                              : CircleAvatar(
+                                  child: Text(
+                                      value.chatUsersList
+                                          .elementAt(index)
+                                          .unreadMessagesCount
+                                          .toString(),
+                                      style: TextStyle(fontSize: 14.sp)),
+                                ),
+                        )
+                      ],
+                    ),
+                  )),
+            );
+          },
+        );
+      }),
+    );
+  }
+}
